baba05c2736efd92925775eea1a72119cb8d712a
sweetforou <sweetforou@gmail.com>
2026-02-27 21:20:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ async function main() {
         continue
       }
       printMagenta(`账号${userDetail?.data?.nickname}开始领取VIP...`)
-      wxpusher()
+
       // 开始听歌
       printYellow(`开始听歌领取VIP...`)
       // 听歌获取vip
